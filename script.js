203d3576733260295c3254b54aca1ec2b930caef
Sean G <seangan.en@Gmail.com>
2022-09-15 10:53:54 +1000

    Etch-a-sketch is working for the first iteration

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function buildSquare(number){
+  for (i = 0; i<number; i++){
+    for (j = 0; j<number; j++){
+      var div = document.createElement("div");
+      div.style.width = '50px';
+      div.style.height = '50px';
+      div.style.background = 'red';
+      div.className = 'pixel';
+      document.getElementById("main").appendChild(div);
+    }
+  }
+}
+
+function changeSquare(event){
+  event.preventDefault();
+    const initial = document.querySelectorAll('.pixel');
+    initial.forEach(item => {
+      item.remove();
+    })
+  const number = document.getElementById('number').value;
+  
+  document.getElementById('main').style.columnCount = number;
+  
+  buildSquare(number);
+  const pixel = document.querySelectorAll('.pixel');
+  pixel.forEach(item => {
+  item.addEventListener('mouseover', function(e){
+      e.target.style.background = 'black';
+  })
+  })
+}
+
